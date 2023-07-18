5840e71a70e364321e5a5e3a2678a578ceb0656e
NaqibKhusairy <naaqibkhusairy@gmail.com>
2023-07-18 16:30:55 +0800

    done until payment

---
+++ app/src/main/java/com/order/foododeringsystem/resit.java
@@ -0,0 +1,14 @@
+package com.order.foododeringsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resit);
+    }
+}
